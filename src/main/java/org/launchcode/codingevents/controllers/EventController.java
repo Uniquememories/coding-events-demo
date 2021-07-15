df2dc6b1735ae83dd5b48ad422f32b8935a10bac
Kimberly Luke <kimberly_luke@cable.comcast.com>
2021-07-15 13:10:00 -0400

    Model Binding up to CH 12.3.5 Video

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,6 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
 import org.apache.coyote.Request;
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,12 +20,12 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
+//    private static List<Event> events = new ArrayList<>(); //How was previously storing events
 
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -37,7 +38,24 @@ public class EventController {
     @PostMapping("create")
     public String processCreateEventForm(@RequestParam String eventName,
                                          @RequestParam String eventDescription) {
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+        if(eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
         return "redirect:";
     }
 
